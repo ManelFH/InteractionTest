e478aefe75cdc1c30c9d8ec1604a9147468e97af
ManelFH <fhidalgo.manel@gmail.com>
2019-04-07 18:37:11 +0200

    Initial commit

---
+++ Assets/Script/DoorKey.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+public class DoorKey : MonoBehaviour
+{
+    //att
+    public bool inTrigger;
+    //Trigger event
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+    //
+    void Update()
+    {
+        if (inTrigger)//if it's inside
+        {
+            if (Input.GetKeyDown(KeyCode.E))//when press E key, the gameobject will be destrolled
+            {                               //change the value to doorKey
+                DoorScript.doorKey = true;
+                Destroy(this.gameObject, 0.5f);
+            }
+        }
+    }
+
+    //GUI
+    void OnGUI()
+    {
+        //TODO
+        if (DoorScript.doorKey)//you get a key, show message
+        {
+            GUI.Box(new Rect(0, 60, 200, 25), "¡You have obtained a key!");
+        }
+        if (inTrigger)//if it's inside
+        {
+            if (!DoorScript.doorKey)//if haven't the key, show message
+            {
+                GUI.Box(new Rect(0, 60, 200, 25), "Press E to take a key");
+            }
+
+        }
+    }
+}
---
+++ Assets/Script/DoorScript.cs
@@ -0,0 +1,76 @@
+﻿using UnityEngine;
+
+public class DoorScript : MonoBehaviour
+{
+    //att
+    public static bool doorKey;
+    public bool inTrigger;
+    private static bool open = false, close = true;
+    //Trigger event
+    void OnTriggerEnter(Collider other)
+    {
+        inTrigger = true;
+    }
+    void OnTriggerExit(Collider other)
+    {
+        inTrigger = false;
+    }
+    //
+    void Update()
+    {
+        if (inTrigger)//if it's inside
+        {
+            if (close)//if the door it's close
+            {
+                if (doorKey)//if have the key
+                {
+                    if (Input.GetKeyDown(KeyCode.E))//when press E key, the gameObject(door)
+                    {                               //change status to closed
+                        open = true;
+                        close = false;
+                    }
+                }
+            }
+            else//the door it's open
+            {
+                if (Input.GetKeyDown(KeyCode.E))//when press E key, the gameObject(door)
+                {                               //change status to opened
+                    close = true;
+                    open = false;
+                }
+            }
+        }
+        if (open)//when the status Open it's true, the gameObject(door) rotate to open it
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, -90.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+        else//when the status Open it's false, the gameObject(door) rotate to close it
+        {
+            var newRot = Quaternion.RotateTowards(transform.rotation, Quaternion.Euler(0.0f, 0.0f, 0.0f), Time.deltaTime * 200);
+            transform.rotation = newRot;
+        }
+    }
+    //GUI
+    void OnGUI()
+    {
+        if (inTrigger)//if it's inside
+        {
+            if (open)//when the status Open it's true, show message
+            {
+                GUI.Box(new Rect(0, 0, 200, 25), "Press E key to close the door");
+            }
+            else//when the status Open it's false
+            {
+                if (doorKey)//if have the key
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "Press E key to open the door");
+                }
+                else
+                {
+                    GUI.Box(new Rect(0, 0, 200, 25), "¡Need a key!");
+                }
+            }
+        }
+    }
+}
